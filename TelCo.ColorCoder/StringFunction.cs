d9f7d5accc8b034e72b3b70d8ef8cb9c52a9a4fc
patreshubham94 <68467015+patreshubham94@users.noreply.github.com>
2020-09-05 22:00:52 +0530

    Update StringFunction.cs

--- TelCo.ColorCoder/StringFunction.cs
+++ TelCo.ColorCoder/StringFunction.cs
@@ -6,13 +6,13 @@ namespace TelCo.ColorCoder
 {
      partial class Program
     {
-         public override void ToString()
+         public override string ToString()
         {
             string str = "";
             for (int i = 1; i <= 25; i++) {
                 str = str + string.Format("PairNumber: {0}, {1}\n", i, Program.GetColorFromPairNumber(i).ToString());
             }
-            Console.WriteLine(str);
+           return str;
         }
     }
 }
